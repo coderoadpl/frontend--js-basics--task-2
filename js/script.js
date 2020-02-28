e29fa3bf7f94c5c4090c9917ad5a1229c2fd2c1a
Mateusz <chomamateusz@gmail.com>
2020-02-28 13:15:32 +0100

    Automatic commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Primitives
+
+// Number
+var numberLiteral = 5
+var numberLiteral2 = 1.5
+// String
+var stringLiteral = 'Hello'
+var stringLiteral2 = "I can't"
+// Boolean
+var booleanLiteral = true
+var booleanLiteral2 = false
+// null
+var nullLiteral = null
+// undefined
+var undefinedLiteral = undefined
+
+// Objects
+
+// Object
+var objectLiteral = {
+    name: 'Fluffy',
+    age: 3
+}
+// Array (subtype or kind of Object)
+var arrayLiteral = [ 'Fluffy', 3 ]
+// Functions are also Objects !!!
